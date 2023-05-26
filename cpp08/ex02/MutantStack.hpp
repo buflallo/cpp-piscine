2cb8c6cb667272a05c6d0b0ccbac81a49e04d974
Hamza Lachkar <hlachkar@hlachkar@student.1337.ma>
2023-05-26 23:45:55 +0100

    ex02 done

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -15,8 +15,25 @@ class MutantStack : public std::stack<T>
         ~MutantStack();
 
         typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+        typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+        typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
         iterator begin();
         iterator end();
+
+        const_iterator begin() const;
+        const_iterator cbegin() const;
+        const_iterator end() const;
+        const_iterator cend() const;
+
+        reverse_iterator rbegin();
+        reverse_iterator rend();
+
+        const_reverse_iterator rbegin() const;
+        const_reverse_iterator crbegin() const;
+        const_reverse_iterator rend() const;
+        const_reverse_iterator crend() const;
 };
 
 #include "MutantStack.tpp"
